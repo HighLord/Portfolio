14ef6a599141ac754d70d24fe92eb89a26141a0a
HighLord <1stempirelord@gmail.com>
2022-11-02 04:22:52 +0100

    Portfolio

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.onload = function()
 {
     var SplashScreen = document.querySelector('.splash');
-    var links = document.getElementsByClassName('wrapper')[0];
+    var links = document.getElementsByClassName('main')[0];
     setTimeout(() => 
     {
         SplashScreen.style.opacity = 0;
         SplashScreen.classList.add('hidden');
         setTimeout(() => 
         {
-            links.style.display = "table-cell";
+            links.style.display = "block";
             SplashScreen.style.display = "none";
         }, 1500);
     }, 2000);
