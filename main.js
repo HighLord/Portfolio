a48aac3b003f1f77353ea2c63e399a717d354002
HighLord <1stempirelord@gmail.com>
2022-11-06 05:09:49 +0100

    Portfolio

--- main.js
+++ main.js
@@ -13,13 +13,20 @@ window.onload = function()
         }, 1500);
     }, 2000);
 
-    $('.display').click(function()
+    const body = document.body;
+    const border = document.getElementsByTagName('div')[2];
+    const enabled = document.getElementsByClassName('l')[0];
+    const disabled = document.getElementsByClassName('d')[0];
+    const light = document.getElementsByClassName('display')[0];
+    light.addEventListener('click', () => 
     {
-        $('body').style('background', 'black');
-    });
-    document.getElementsByClassName("display").addEventListener("click", function()
-    {
-        document.getElementsByTagName("body").style.background = "black"; 
-       
-    });
+        const set_color = body.getAttribute('class') === 'dark' ? 'light' : 'dark';
+        body.setAttribute('class', set_color);
+        const set_border = border.getAttribute('class') === 'border container' ? 'no-border container' : 'border container';
+        border.setAttribute('class', set_border);
+        const enable = enabled.getAttribute('class') === 'fa-solid fa-square lcode' ? 'fa-solid fa-square dcode' : 'fa-solid fa-square lcode';
+        enabled.setAttribute('class', enable);
+        const disable = enabled.getAttribute('class') === 'fa-solid fa-square dcode' ? 'fa-solid fa-square lcode' : 'fa-solid fa-square dcode';
+        disabled.setAttribute('class', disable);
+    })
 };
