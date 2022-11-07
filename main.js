e33a1a70def760cfd952d055f3ca0ba6e08359f3
HighLord <1stempirelord@gmail.com>
2022-11-07 08:09:28 +0100

    Portfolio

--- main.js
+++ main.js
@@ -27,15 +27,15 @@ window.onload = function()
         const set_color = body.getAttribute('class') === 'dark' ? 'light' : 'dark';
         if (container.style.backgroundColor == 'linear-gradient(-45deg, #a8acb5, #8889c47e, #5d3f4364, #d1d0d0e0, rgba(255, 255, 255, 0.861))')
         {
-            alert('yes');
+            alert(style.background);
         }
         else
         {
-            alert('no');
+            alert(style.background);
         }
-        const set_bg = container.getAttribute('style.background') === 'linear-gradient(-45deg, #a8acb5, #8889c47e, #5d3f4364, #d1d0d0e0, rgba(255, 255, 255, 0.861))' ? 'linear-gradient(-45deg, #130606, #261b1bbe, #040345de, #020223c4, rgba(53, 10, 10, 0.818))' : 'linear-gradient(-45deg, #a8acb5, #8889c47e, #5d3f4364, #d1d0d0e0, rgba(255, 255, 255, 0.861))';
+        const set_bg = container.getAttribute('style.backgroundColor') === 'linear-gradient(-45deg, #a8acb5, #8889c47e, #5d3f4364, #d1d0d0e0, rgba(255, 255, 255, 0.861))' ? 'linear-gradient(-45deg, #130606, #261b1bbe, #040345de, #020223c4, rgba(53, 10, 10, 0.818))' : 'linear-gradient(-45deg, #a8acb5, #8889c47e, #5d3f4364, #d1d0d0e0, rgba(255, 255, 255, 0.861))';
         body.setAttribute('class', set_color);
-        container.setAttribute('style.background', set_bg);
+        container.setAttribute('style.backgroundColor', set_bg);
         const set_border = border.getAttribute('class') === 'border container' ? 'no-border container' : 'border container';
         border.setAttribute('class', set_border);
         const enable = enabled.getAttribute('class') === 'fa-solid fa-square lcode' ? 'fa-solid fa-square dcode' : 'fa-solid fa-square lcode';
