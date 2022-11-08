0b6b1c12c015e40635d5c3ddace254777624dc06
HighLord <1stempirelord@gmail.com>
2022-11-08 03:28:59 +0100

    Portfolio

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ window.onload = function()
         }, 1500);
     }, 2000);
 
-    const container = document.getElementsByClassName('container')[0];
+    const container = document.styleSheets[0].cssRules[12].style;
     const border = document.getElementsByTagName('div')[2];
     const enabled = document.getElementsByClassName('l')[0];
     const disabled = document.getElementsByClassName('d')[0];
@@ -25,12 +25,11 @@ window.onload = function()
         body.style.background = 'black';
         const set_color = body.getAttribute('class') === 'dark' ? 'light' : 'dark';
         body.setAttribute('class', set_color);
-        
-        //var cons = container.style.getPropertyValue('background-image');
-        //console.log(cons);
-        //const set_bg = window.getComputedStyle(container).getPropertyValue('background-image') === 'linear-gradient(-45deg, rgb(168, 176, 196), rgba(106, 106, 107, 0.978), rgba(93, 63, 81, 0.634), rgba(227, 227, 227, 0.527), rgba(160, 166, 168, 0.627))' ? 'linear-gradient(-45deg, rgb(139, 26, 26), rgba(52, 122, 182, 0.745), rgba(22, 20, 156, 0.871), rgba(51, 198, 169, 0.769), rgba(17, 4, 4, 0.818))' : 'linear-gradient(-45deg, rgb(168, 176, 196), rgba(106, 106, 107, 0.978), rgba(93, 63, 81, 0.634), rgba(227, 227, 227, 0.527), rgba(160, 166, 168, 0.627))';
-        //container.style.setPropertyValue('background-image', set_bg, null);
-        //container.style.setAttribute('backgroundImage', set_bg);
+        let oldColor = 'linear-gradient(-45deg, rgb(168, 176, 196), rgba(106, 106, 107, 0.976), rgba(93, 63, 81, 0.635), rgba(227, 227, 227, 0.525), rgba(160, 166, 168, 0.627)) 0% 0% / 400% 100%';
+        let newColor = 'linear-gradient(-45deg, rgb(139, 26, 26), rgba(52, 122, 182, 0.745), rgba(22, 20, 156, 0.871), rgba(51, 198, 169, 0.769), rgba(17, 4, 4, 0.818)) 0% 0% / 400% 100%';
+        const get_bg = container.background === oldColor ? newColor : oldColor;
+        container.setProperty("background", get_bg);
+        console.log(get_bg);
         const set_border = border.getAttribute('class') === 'border container' ? 'no-border container' : 'border container';
         border.setAttribute('class', set_border);
         const enable = enabled.getAttribute('class') === 'fa-solid fa-square lcode' ? 'fa-solid fa-square dcode' : 'fa-solid fa-square lcode';
