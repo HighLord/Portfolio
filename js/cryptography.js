3ac77914b4c0909ab4c6186ab3dedba5b4ab5149
HighLord <1stempirelord@gmail.com>
2022-11-12 07:00:10 +0100

    Portfolio

--- js/cryptography.js
+++ js/cryptography.js
@@ -1,9 +1,18 @@
 window.onload = function()
 {
-    const notification = document.getElementsByClassName('fa-bell')[0];
-    notification.addEventListener('click', () =>
+    const bell = document.getElementsByClassName('fa-bell')[0];
+    const body = document.getElementsByTagName('body')[0];
+    const notification = document.getElementsByClassName('notify-text')[0];
+    bell.addEventListener('click', () =>
     {
-        console.log('works');
+        $(notification).toggle('500', function()
+        {
+            $('body>*:not(.notify)').css('filter', 'blur(5px)');
+            setTimeout(() => {
+                $('body>*:not(.notify)').css('filter', 'blur(0px)'); 
+                $(notification).css('visibility', 'hidden');  
+            }, 3000);
+        });
         
     });
 }
