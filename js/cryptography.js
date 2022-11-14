97b0500c33bb01f6cfffa39ca92487642a1a930b
HighLord <1stempirelord@gmail.com>
2022-11-14 06:26:41 +0100

    Portfolio

--- js/cryptography.js
+++ js/cryptography.js
@@ -13,7 +13,7 @@ window.onload = function()
             {
                 if (amount == 1)
                 {
-                    $('.notify').css('z-index', '0');
+                    $('.notify').css('z-index', '1');
                     $('body>*:not(.notify, .fa-bell, .count)').css('filter', 'blur(5px)');
                     amount = 2
                 }
