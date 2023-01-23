b2071ddd5984bcde480d1af879c05325e9de63ca
HighLord <1stempirelord@gmail.com>
2023-01-23 13:25:48 +0100

    portfolio

--- js/sports.js
+++ js/sports.js
@@ -1,12 +1,20 @@
 window.onload = function()
 {
     var button1 = document.getElementById('button1');
+    var div1 = document.getElementById('div1');
+    var div2 = document.getElementById('div2');
+    var button2 = document.getElementById('button2');
 
     button1.addEventListener('click', () =>
     {
         setTimeout(() =>
         {
             button1.innerHTML = '<i class="fa fa-spinner fa-spin"></i>';
-        }, 2000);
+            setTimeout(() =>
+            {
+                div1.style.display = 'none';
+                div2.style.display = 'flex';
+            }, 2000);
+        }, 500);
     });
 }
