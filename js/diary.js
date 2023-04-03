decc6f7e3c366006fd909585b880d84670c360cb
HighLord <1stempirelord@gmail.com>
2023-04-03 03:32:41 +0100

    workouts

--- js/diary.js
+++ js/diary.js
@@ -1,5 +1,18 @@
 window.onload = function()
 {
+    // Getting html vars
+    const welSec = document.getElementById('welcome-section');
+    const logSec = document.getElementById('login-section');
+    const loginButton = document.getElementById('login-act');
+    const login = document.getElementById('login');
+    const registerButton = document.getElementById('register-act');
+    const register = document.getElementById('register');
+    const password = document.getElementById('password');
+    const email = document.getElementById('email');
+    const secLogin = document.getElementById('sel-login');
+    const secRegister = document.getElementById('sel-register');
+
+    // setting the current date
     const newDate = document.getElementById('newDate');
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -7,4 +20,31 @@ window.onload = function()
     var yyyy = today.getFullYear();
     today = yyyy + '-' + dd + '-' + mm;
     newDate.value = today;
+
+    // reading the hash of the url
+    var url = new URL(document.URL);
+    var hash = url.hash;
+    console.log(hash);
+
+    // read cookies
+    var cookies = document.cookie;
+    if (cookies === '')
+    {
+        welSec.style.display = 'block';
+        logSec.style.display = 'block';
+        loginButton.addEventListener('click', () =>
+        {
+            logSec.style.display = 'none';
+            login.style.display = 'block';
+            password.focus()
+        });
+        registerButton.addEventListener('click', () =>
+        {
+            logSec.style.display = 'none';
+            register.style.display = 'block';
+            email.focus();
+        });
+    }
+    console.log(cookies);
+    
 }
