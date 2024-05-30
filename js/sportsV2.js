fcddd4ec048531eb38d1901a4f272b9a9153075f
Lawrence <1stempirelord@gmail.com>
2024-05-30 08:55:42 +0100

    logging

--- js/sportsV2.js
+++ js/sportsV2.js
@@ -208,7 +208,8 @@ function myFunction ()
                                                         delete dataKing[key];
                                                     }
                                                 });
-
+                                                console.log('total='+total+' amtOfBooking='+amountOfBooking+' selectV2='+selectv2);
+                                                
                                                 if (total < amountOfBooking && selectv2 === null && book === false && bypass === false && stop === false)
                                                 {
                                                     amountOfBooking = parseFloat(amountOfBooking);
