4379ccd370a4ee892a0f11949302f149a34e7e1d
Lawrence <1stempirelord@gmail.com>
2024-12-02 01:01:36 +0100

    "Uncommented console.log statement in myFunction()"

--- js/sportsV2.js
+++ js/sportsV2.js
@@ -1167,7 +1167,7 @@ function myFunction ()
             result = ( draw1 === true && draw2 === true && draw3 === true ) ? true : null;
         }
 
-        //console.log( "sum1: " + sum1 + " and sum2: " + sum2 + " score1: " + score1 + " and score2: " + score2 + "  master: " + master +  " head2head: " + head2head + " result: " + result + " draws: " + draw1 + draw2 + draw3 );
+        console.log( "sum1: " + sum1 + " and sum2: " + sum2 + " score1: " + score1 + " and score2: " + score2 + "  master: " + master +  " head2head: " + head2head + " result: " + result + " draws: " + draw1 + draw2 + draw3 );
         calculatedJson.result = result;
         return result !== null ? calculatedJson : null;
     }
